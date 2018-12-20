568be3a5fcef4134b1036f2747be208e707cd093
Theo Syslack <theosyslack@Theos-MacBook-Pro-2.local>
2018-12-20 16:37:16 -0600

    show error if you don't pass travel-plan

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,36 @@
 import { sendSleuth } from "./actions/sleuth";
-import { globe } from "./actions/log";
-
-// add asyncIterator functionality - https://github.com/Microsoft/TypeScript/issues/14151
-(<any>Symbol).asyncIterator =
-    Symbol.asyncIterator || Symbol.for("Symbol.asyncIterator");
+import log, { globe } from "./actions/log";
 
 const main = async (args: string[]) => {
-    const [action, pathToTravelPlan] = args;
-    console.clear();
-    console.log(action, pathToTravelPlan);
-    switch (action) {
-        default:
-            const {
-                default: { destinations: travelPlan }
-            } = await import(pathToTravelPlan);
-
-            runMissions(travelPlan);
-            break;
-    }
+  const [action, pathToTravelPlan] = args;
+  console.clear();
+  console.log(action, pathToTravelPlan);
+  switch (action) {
+    case "follow":
+      const {
+        default: { destinations: travelPlan }
+      } = await import(pathToTravelPlan);
+      runMissions(travelPlan);
+      break;
+    default:
+      log("You have to pass in a travel plan.", "error");
+      log("yarn start follow travel-plan.ts", "pending");
+      break;
+  }
 };
 
 async function runMissions(travelPlan) {
-    let results: any[] = [];
-    for await (const destination of travelPlan) {
-        const responses = sendSleuth(destination.missions, destination.url);
-        for await (const response of responses) {
-            results.push(response);
-        }
+  let results: any[] = [];
+  for await (const destination of travelPlan) {
+    const responses = sendSleuth(destination.missions, destination.url);
+    for await (const response of responses) {
+      results.push(response);
     }
+  }
 
-    // TODO: Something cooler than console.log
-    // console.log(results);
-    globe();
+  // TODO: Something cooler than console.log
+  // console.log(results);
+  globe();
 }
 
 main(process.argv.splice(2));

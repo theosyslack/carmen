f425ff203aa0b2bc77c153d7121d9a36ae19a1f5
Theo Syslack <theosyslack@Theos-MacBook-Pro-2.local>
2018-12-20 11:08:51 -0600

    add edexample for grabbing travel plans

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import takeScreenshot from "./missions/takeScreenshot";
 
 const main = async (args: string[]) => {
-  const [action] = args;
-
+  const [action, pathToTravelPlan] = args;
+  console.clear();
+  console.log(action, pathToTravelPlan);
   switch (action) {
     default:
+      const { default: travelPlan } = await import(pathToTravelPlan);
+      console.table(travelPlan);
       break;
   }
 };

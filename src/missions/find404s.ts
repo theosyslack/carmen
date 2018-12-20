7d8fddebc9bce8d119ccba16d0898376ed2d2ae7
npetes <npeters@crescendocollective.com>
2018-12-20 14:13:50 -0600

    further 404 work

--- src/missions/find404s.ts
+++ src/missions/find404s.ts
@@ -1,36 +1,62 @@
 import * as puppeteer from 'puppeteer';
+import { URL } from 'url';
+import log from "../actions/log";
+
+let targetPage = 'https://retail.johnsonville.com'
 
 export default async () => {
+    let failedStatus: any[] = [];
+    let errorPages: any[] = [];
+
     const browser = await puppeteer.launch({
         headless: true
     });
     const page = await browser.newPage();
-    await page.goto('https://retail.johnsonville.com');
+    await page.goto(targetPage);
 
     let resultAnchors = await page.evaluate(() => {
         let elements = Array.from(document.querySelectorAll('a')).map(link => link.href)
-        return elements; // Return our data array
+        return elements;
     });
 
-    console.log(resultAnchors);
+    log(resultAnchors.length + ' links found on ' + targetPage, 'pending')
 
-    // for (let anchor of resultAnchors) {
-    //     await page.goto(anchor);
-    //     await page.on('response', response => {
-    //         if (response.status() > 399)
-    //             console.log("response code: ", anchor, response.status());
-    //         // do something here
-    //     });
-    //     // let response = await page.on('response', resp => {
-    //     //     var header = resp.headers();
-    //     //     return header['content-disposition'];
-    //     // });
-    //     // console.log(response);
-    // }
+    const start = async () => {
+        await asyncForEach(resultAnchors, async (anchor) => {
+            try {
+                let { origin, pathname, search } = new URL(anchor)
+                let sanitizedURL = origin + pathname + search;
+
+                log('Navigating to: ' + sanitizedURL, 'default')
+
+                const response = await page.goto(sanitizedURL, {
+                    waitUntil: 'domcontentloaded',
+                    timeout: 6000
+                });
+                if (response.status() > 399)
+
+                    failedStatus.push(sanitizedURL, response.status());
+
+            } catch (error) {
+                log('Error on page: ' + anchor + error, 'error')
+                errorPages.push(anchor, error);
+            }
+
+        });
+
+        console.log('Done');
+        console.log('failedStatus', failedStatus);
+        console.log('errorPages', errorPages);
+        await browser.close();
+    }
+    start();
 
 
+    async function asyncForEach(array, callback) {
+        for (let index = 0; index < array.length; index++) {
+            await callback(array[index], index, array);
+        }
+    }
 
 
-
-    await browser.close();
 };

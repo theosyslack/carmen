e1d77c2eed459b4295abc9c1957446c8f9054827
Theo Syslack <theosyslack@theo-work-mbpr.local>
2019-01-07 19:41:41 -0600

    Pass browser through to runMissions

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { sendSleuth } from "./actions/sleuth";
+import * as puppeteer from "puppeteer";
 import log from "./actions/log";
 import * as path from "path";
 import { TravelPlan } from ".";
@@ -6,7 +7,10 @@ import { TravelPlan } from ".";
 const main = async (args: string[]) => {
   const [action = "follow", relativePathToTravelPlan = "travel-plan.js"] = args;
   console.clear();
+
+  const browser = await puppeteer.launch();
   const pathToTravelPlan = path.join(process.cwd(), relativePathToTravelPlan);
+
   switch (action) {
     case "follow":
     default:
@@ -17,6 +21,10 @@ const main = async (args: string[]) => {
           `Traveling to ${travelPlan.destinations.length} destinations.`,
           "pending"
         );
+
+        const missionResults = await runMissions(travelPlan, browser);
+        log(missionResults);
+        await browser.close();
       } catch (e) {
         if (e.code === "MODULE_NOT_FOUND") {
           log(`No plan found at ${pathToTravelPlan}. ${e.code}`, "error");
@@ -24,19 +32,28 @@ const main = async (args: string[]) => {
           log(e, "error");
         }
       }
+
       break;
   }
 };
 
-async function runMissions(travelPlan) {
+async function runMissions(
+  { destinations }: TravelPlan,
+  browser: puppeteer.Browser
+) {
   let results: any[] = [];
-  for await (const destination of travelPlan) {
-    const responses = sendSleuth(destination.missions, destination.url);
-    for await (const response of responses) {
-      results.push(response);
-    }
-  }
 
+  await Promise.all(
+    destinations.map(async destination => {
+      const page = await browser.newPage();
+      const responses = sendSleuth(destination, page);
+      for await (const response of responses) {
+        results.push(response);
+      }
+    })
+  );
+
+  return results;
   // TODO: Something cooler than console.log
   // console.log(results);
 }

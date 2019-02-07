c23e74cedb331fe22763e6a9834e4540e5476af2
Theo Syslack <tsyslack@crescendocollective.com>
2019-02-07 15:57:06 -0600

    Add saveReport utilities

--- src/actions/file.ts
+++ src/actions/file.ts
@@ -1,5 +1,6 @@
 import { join as joinPath, resolve as resolvePath, parse } from "path";
 import { promisify } from "util";
+import { curry } from "ramda";
 import * as fs from "fs";
 import log from "./log";
 export const access = promisify(fs.access);
@@ -12,29 +13,25 @@ export const sanitize = string => {
   //TODO: Write a sanitize function to make filename safe
 };
 
-export const exists = async path => {
-  return await access(path, fs.constants.F_OK)
-    .then(() => true)
-    .catch(err => {
-      return false;
-    });
-};
+////////////
+/// Create
 
-export const getContents = async path => {
-  const fullPath = joinPath(path);
-
-  return await read(fullPath, { encoding: "utf-8" });
-};
-
-export const writeObjectToFile = async (object, filePath: string) => {
-  if (!filePath.includes(".json")) {
-    filePath += ".json";
+export const writeObjectToFile = curry(
+  async (object: object, filePath: string) => {
+    if (!filePath.endsWith(".json")) {
+      filePath += ".json";
+    }
+    const resolvedPath = resolvePath(process.cwd(), filePath);
+    await createFolderForFile(resolvedPath);
+    log(`Saved ${filePath}`, "success");
+    return await write(resolvedPath, JSON.stringify(object, null, 2)).catch(
+      e => {
+        log(`Failed to save file to: ${filePath} with error: ${e}`);
+        return e;
+      }
+    );
   }
-  const resolvedPath = resolvePath(process.cwd(), filePath);
-  await createFolderForFile(resolvedPath);
-  log(`Saved ${filePath}`, "success");
-  return await write(resolvedPath, JSON.stringify(object, null, 2));
-};
+);
 
 export const createFolderForFile = async (filePath: string) => {
   const { dir } = parse(filePath);
@@ -50,3 +47,37 @@ export const createFolderForFile = async (filePath: string) => {
     }
   });
 };
+
+export const writeReport = (
+  reportType: string = "misc",
+  filePath: string = Date.now().toString(),
+  object: object = {}
+) =>
+  writeObjectToFile(object, `./carmen-reports/${reportType}/${filePath}.json`);
+
+export const createReportWriter = (
+  reportType: string,
+  filePath: string
+) => object => writeReport(reportType, filePath, object);
+
+////////////
+/// Read
+export const getContents = async path => {
+  const fullPath = joinPath(path);
+
+  return await read(fullPath, { encoding: "utf-8" });
+};
+
+export const exists = async path => {
+  return await access(path, fs.constants.F_OK)
+    .then(() => true)
+    .catch(err => {
+      return false;
+    });
+};
+
+////////////
+/// Update
+
+////////////
+/// Delete

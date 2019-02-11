bf202009868e12117f812929e337fb51a969933b
Theo Syslack <tsyslack@crescendocollective.com>
2019-02-11 12:12:35 -0600

    add exports for new Missions

--- src/missions/index.ts
+++ src/missions/index.ts
@@ -2,3 +2,5 @@ export { default as find404s } from "./find404s";
 export { default as findLargeMedia } from "./findLargeMedia";
 export { default as getPageSize } from "./getPageSize";
 export { default as takeScreenshot } from "./takeScreenshot";
+export { default as findAFew404s } from "./findAFew404s";
+export { default as checkForADACompliance } from "./checkForADACompliance";

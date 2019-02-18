b6cab5b294d6d9b482e03e0625646e0f82f374b3
Theo Syslack <tsyslack@crescendocollective.com>
2019-02-18 10:57:10 -0600

    add "summary" ability to checkForADACompliance

--- src/missions/checkForADACompliance.ts
+++ src/missions/checkForADACompliance.ts
@@ -1,21 +1,32 @@
 import log from "../actions/log";
 import * as puppeteer from "puppeteer";
-import { createReportWriter } from "../actions/file";
+import { createReportWriter, write, writeToNewFile } from "../actions/file";
 import { AxePuppeteer } from "axe-puppeteer";
 import { Mission } from "..";
+import { createFolderPathFromUrl } from "../common/utilities";
+import { join } from "path";
+import { AxeResults } from "axe-core";
+import { summarizeAxeResults } from "../actions/compare";
+
 const checkForADACompliance = async (page: puppeteer.Page) => {
   let result;
   log("Checking for ADA issues...", "pending");
 
-  const results = await new AxePuppeteer(page).analyze();
+  const url = await page.url();
+  const folder = join("checkForADACompliance", createFolderPathFromUrl(url));
+
+  const results: AxeResults = await new AxePuppeteer(page).analyze();
   log(`${results.violations.length} issues found.`, "pending");
 
-  const writeReport = createReportWriter(
-    "checkForADACompliance",
-    Date.now().toString()
-  );
+  const now = Date.now().toString();
 
-  await writeReport(results); //TODO: Update to use URL.
+  const writeReport = createReportWriter(folder, now);
+
+  const summarizedResults = summarizeAxeResults(results);
+
+  await writeToNewFile(`./${folder}/test.md`, summarizedResults);
+
+  await writeReport(results);
   return result;
 };
 

d4f8a4e2b4bf20b6c939c05d8828633f96e235f0
Theo Syslack <tsyslack@crescendocollective.com>
2019-02-18 10:56:35 -0600

    add utils for making folder paths and getting URLS

---
+++ src/common/utilities.ts
@@ -0,0 +1,42 @@
+import { pick } from "ramda";
+import * as path from "path";
+
+export type URLLike = URL | string;
+
+export const createFolderPathFromUrl = (urlLike: URLLike) => {
+  const { hostname, pathname } = createURLFromURLLike(urlLike);
+  let folderPath = "";
+  const extension = path.extname(pathname);
+
+  if (extension === "") {
+    folderPath = path.join(hostname, pathname);
+  } else {
+    folderPath = path.join(hostname, pathname.replace(extension, ""));
+  }
+  return path.posix.normalize(folderPath);
+};
+
+export const getPropertiesFromUrl = (
+  urlLike: URLLike,
+  properties: string[]
+) => {
+  const url = createURLFromURLLike(urlLike);
+
+  return pick(properties, url);
+};
+
+export const createURLFromURLLike = (urlLike: URLLike): URL => {
+  if (urlLike instanceof URL) {
+    return urlLike;
+  } else {
+    return createURLFromString(urlLike);
+  }
+};
+
+export const createURLFromString = (string): URL => {
+  try {
+    return new URL(string);
+  } catch {
+    throw new Error(`"${string}" is not a URL.`);
+  }
+};

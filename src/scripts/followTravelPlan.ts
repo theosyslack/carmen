f65564c00e12f4683ebb63e08009799d371c53b3
Theo Syslack <tsyslack@crescendocollective.com>
2019-08-22 11:34:06 -0500

    Run tasks synchronously (eventually implement queuing)

--- src/scripts/followTravelPlan.ts
+++ src/scripts/followTravelPlan.ts
@@ -8,8 +8,32 @@ const followTravelPlan = async (plan: TravelPlan) => {
   const runDestination = createDestinationRunner(browser);
   const { destinations } = plan;
 
+  let count = 0;
+  let start = Date.now();
+
   log(`Traveling to ${destinations.length} destinations.`, "success");
-  await Promise.all(destinations.map(runDestination));
+  for (const destination of destinations) {
+    console.clear();
+    const estimateTimeRemaining = () => {
+      let duration = (Date.now() - start) / 1000;
+      let secondsPerTask = duration / count;
+
+      let tasksRemaining = destinations.length - count;
+      let secondsRemaining = Number.parseFloat(
+        `${secondsPerTask * tasksRemaining}`
+      ).toFixed();
+      // return duration / 1000 / amountDone;
+
+      return count > 1 ? `(${secondsRemaining}s remaining)` : "";
+    };
+    count++;
+    log(
+      `${count} of ${destinations.length} ${estimateTimeRemaining()}`,
+      "success"
+    );
+    await runDestination(destination);
+  }
+  // await Promise.all(destinations.map());
   log(`Travel plan completed.`, "success");
 
   await browser.close();

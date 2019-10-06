ecf81b14f316f99eb3dfae2ab6213113dc6060fe
Theo Syslack <tsyslack@crescendocollective.com>
2019-10-06 17:33:10 -0500

    refactor createMission to return MissionReport

--- src/helpers/mission.ts
+++ src/helpers/mission.ts
@@ -4,42 +4,57 @@ import {
   Mission,
   MissionResult
 } from "../types/carmen";
+import { writeObjectToFile } from "./file";
 
 const createMissionReport = (
   name: string,
+  path: string,
   status: MissionStatus = "IN QUEUE",
-  result: MissionResult
+  { result, context }: MissionResult
 ): MissionReport => {
   const timestamp = new Date();
-
-  const report = {
+  const fullPath = path + "report.json";
+  const writtenReport = {
     name,
+    path,
     status,
     timestamp,
     result
   };
 
-  return report;
+  const fullReport = Object.assign({}, writtenReport, { context });
+
+  writeObjectToFile(fullPath, writtenReport);
+
+  return fullReport;
+};
+
+const onMissionSuccess = (name: string, path: string, result: MissionResult) =>
+  createMissionReport(name, path, "SUCCESS", result);
+
+const onMissionFailure = (name: string, path: string, { message }: Error) => {
+  switch (message) {
+    case "Protocol error (Page.navigate): Cannot navigate to invalid URL":
+      return createMissionReport(name, path, "FAILURE", {
+        result: { error: `Not a valid URL.` },
+        context: {}
+      });
+    default:
+      return createMissionReport(name, path, "FAILURE", {
+        result: { error: message },
+        context: {}
+      });
+  }
 };
 
 export const createMission = (
   name: string = "Generic Mission",
-  callback: () => Promise<object>
+  path: string = "./reports/Generic",
+  callback: () => Promise<MissionResult>
 ): Mission => {
   return (...args) => {
     return callback(...args)
-      .then(result => createMissionReport(name, "SUCCESS", result))
-      .catch(({ code, message }) => {
-        switch (message) {
-          case "Protocol error (Page.navigate): Cannot navigate to invalid URL":
-            return createMissionReport(name, "FAILURE", {
-              error: `Not a valid URL.`
-            });
-          default:
-            return createMissionReport(name, "FAILURE", {
-              error: message
-            });
-        }
-      });
+      .then(result => onMissionSuccess(name, path, result))
+      .catch(error => onMissionFailure(name, path, error));
   };
 };

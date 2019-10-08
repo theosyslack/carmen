49603f6f64912676dbe0c607f95d888febef06d6
Theo Syslack <tsyslack@crescendocollective.com>
2019-10-08 11:16:35 -0500

    export helpers, too

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import run from "./commands/run";
 import saveScreenshot from "./missions/saveScreenshot";
 import compareScreenshots from "./missions/compareScreenshots";
+import * as file from "./helpers/file";
+import * as log from "./helpers/log";
 
 const missions = {
   saveScreenshot,
   compareScreenshots
 };
 
-export { run, missions };
+const helpers = {
+  file,
+  log
+};
+
+export { run, missions, helpers };

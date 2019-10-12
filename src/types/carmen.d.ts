fa6da2dd955986b26df9ebb80631e70b9245756f
Theo Syslack <tsyslack@crescendocollective.com>
2019-10-12 11:08:24 -0500

    require Browsers for Missions

--- src/types/carmen.d.ts
+++ src/types/carmen.d.ts
@@ -1,4 +1,4 @@
-import { Page } from "puppeteer";
+import { Page, Browser } from "puppeteer";
 
 export type Sleuth<T> = (config: SleuthConfig) => Promise<T>;
 
@@ -6,7 +6,7 @@ export interface SleuthConfig {
   page: Page;
 }
 
-export type Mission = () => Promise<MissionReport>;
+export type Mission = (browser: Browser) => Promise<MissionReport>;
 export type MissionFactory = (...args: any[]) => Mission;
 
 export type MissionStatus = "IN QUEUE" | "PENDING" | "SUCCESS" | "FAILURE";

ad8601667562379feb705a4d7fa20c976bee4914
Theo Syslack <tsyslack@crescendocollective.com>
2019-10-12 11:11:31 -0500

    add missions index

---
+++ src/missions/index.ts
@@ -0,0 +1,9 @@
+import saveScreenshot from "./saveScreenshot";
+import compareScreenshots from "./compareScreenshots";
+import findBrokenLinks from "./findBrokenLinks";
+
+export default {
+  saveScreenshot,
+  compareScreenshots,
+  findBrokenLinks
+};

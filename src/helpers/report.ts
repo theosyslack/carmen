427a136a4ac03c2021e39509f309119a10e88475
Theo Syslack <tsyslack@crescendocollective.com>
2019-10-16 10:41:43 -0500

    update blank report to reflect changes

--- src/helpers/report.ts
+++ src/helpers/report.ts
@@ -1,9 +1,10 @@
-import { MissionReport } from "../types/carmen";
+import { MissionReport, MissionConfig } from "../types/carmen";
 
-export const createBlankReport = (): MissionReport => {
+export const createBlankReport = (config?: MissionConfig): MissionReport => {
   const timestamp = new Date();
   return {
     timestamp,
+    config,
     status: "PENDING",
     payload: {}
   };

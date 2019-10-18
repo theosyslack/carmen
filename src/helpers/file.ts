962bd8e1ece598556813130c2b2a7feff9ec45c1
Theo Syslack <tsyslack@crescendocollective.com>
2019-10-18 16:43:04 -0500

    fix createFolderPath

--- src/helpers/file.ts
+++ src/helpers/file.ts
@@ -88,6 +88,7 @@ export const createReportWriter = (
 export const createFolderPathFromUrl = (url: string) => {
   const [urlWithoutParams] = url.split("?");
   const cleanUrl = urlWithoutParams
+    .replace(".", "/")
     .replace("http://", "")
     .replace("https://", "");
   return joinPath(cleanUrl, "/");

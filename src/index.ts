2246f8a2e4dcf99fc91959ceedf0d720a7b6757d
Theo Syslack <tsyslack@crescendocollective.com>
2019-10-22 12:12:15 -0500

    fix build going to wrong directory

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import run from "./commands/run";
 import missions from "./missions";
-import { version } from "../package.json";
+
+const { version } = require("../package.json");
 
 export { run, missions, version };

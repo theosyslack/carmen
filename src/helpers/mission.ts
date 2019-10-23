942bfbd5454be5bfd1311b475ca44563bf7f455c
Theo Syslack <tsyslack@crescendocollective.com>
2019-10-23 16:41:37 -0500

    add more helpful error messaging for missions and always promisify

--- src/helpers/mission.ts
+++ src/helpers/mission.ts
@@ -14,6 +14,7 @@ import getEventEmitter from "../events/getEvents";
 export const createMission = (
   config: MissionConfig
 ): Promise<RunnableMission> => {
+  debugger;
   const isMissionValid = config.mission && typeof config.mission === "function";
   const isNameValid = typeof config.name === "string";
   const isPathValid =
@@ -22,7 +23,23 @@ export const createMission = (
   const isValid = isMissionValid && isNameValid && isPathValid;
 
   if (!isValid) {
-    console.table({ isMissionValid, isNameValid, isPathValid });
+    console.table([
+      {
+        Title: "Is Mission Valid?",
+        Value: isMissionValid,
+        Item: config.mission
+      },
+      {
+        Title: "Is Name Valid?",
+        Value: isNameValid,
+        Item: config.name
+      },
+      {
+        Title: "Is Path Valid?",
+        Value: isPathValid,
+        Item: config.path
+      }
+    ]);
     log(`Looks like you've got a misconfigured mission.`, "error");
 
     process.exit(1);
@@ -57,41 +74,37 @@ const constructMission = async (
 
   return () => {
     events.emit("mission-started", config);
-    events.on("what", () => {
-      console.log("listening");
-    });
-    const { mission } = config;
+    let { mission } = config;
     // console.log("Is Mission promise:", types.isPromise(async () => {}));
 
     if (mission.constructor.name !== "AsyncFunction") {
-      // If it is not Async
-      try {
-        const result = mission(payload);
-        events.emit("mission-success", result);
-        return report.update({ status: "SUCCESS", payload: result });
-      } catch (error) {
-        events.emit("mission-failure", error);
-        return report.update({ status: "FAILURE", payload: { error } });
-      }
-    } else {
-      return mission(payload).then(
-        payload => {
-          events.emit("mission-success", config);
-          return report.update({
-            status: "SUCCESS",
-            payload
-          });
-        },
-        error => {
-          events.emit("mission-failure", config);
-          return report.update({
-            status: "FAILURE",
-            payload: {
-              error
-            }
-          });
-        }
-      );
+      mission = async payload => config.mission(payload);
     }
+
+    return mission(payload).then(
+      async payload => {
+        const latestReport = await report.update({
+          status: "SUCCESS",
+          payload
+        });
+        await page.close();
+        events.emit("mission-success", latestReport);
+        return latestReport as Promise<MissionReport>;
+      },
+      async (error: Error) => {
+        const latestReport = await report.update({
+          status: "FAILURE",
+          error: error.toString(),
+          payload: {
+            stack: error.stack,
+            name: error.name,
+            message: error.message
+          }
+        });
+        events.emit("mission-failure", latestReport);
+        await page.close();
+        return latestReport as Promise<MissionReport>;
+      }
+    );
   };
 };

569fcc9c9c2dd5f6c18303b1c8cbe002ac25f141
Theo Syslack <tsyslack@crescendocollective.com>
2019-10-23 22:42:41 -0500

    do not have headless by default

--- examples/basic.js
+++ examples/basic.js
@@ -4,14 +4,11 @@ const { BasicMission } = carmen.missions;
 
 (async () => {
   await new Promise(res => setTimeout(res, 2000));
-  const result = await carmen.run(
-    [
-      BasicMission({ url: "https://github.com/" }),
-      BasicMission({ url: "https://github.com/" }),
-      BasicMission({ url: "https://github.com/" }),
-      BasicMission({ url: "https://github.com/" }),
-      BasicMission({ url: "https://github.com/" })
-    ],
-    { launchOptions: { headless: true } }
-  );
+  const result = await carmen.run([
+    BasicMission({ url: "https://github.com/" }),
+    BasicMission({ url: "https://github.com/" }),
+    BasicMission({ url: "https://github.com/" }),
+    BasicMission({ url: "https://github.com/" }),
+    BasicMission({ url: "https://github.com/" })
+  ]);
 })();
